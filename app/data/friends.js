d61a3cfc19a7fa1522d8b72e057e0a5fe6ddbf89
BlastedFool <sports_king_10@hotmail.com>
2018-09-15 23:52:12 -0400

    working friend thingy with one default match

---
+++ app/data/friends.js
@@ -0,0 +1,6 @@
+// Includes a single default profile, so that anyone demoing from the GitHub repository will have a result.
+module.exports = [{
+    "name": "Misty",
+    "photo": "https://cdn.bulbagarden.net/upload/thumb/b/b1/Misty_AG.png/200px-Misty_AG.png",
+    "answers": ["2", "5", "5", "4", "5", "1", "1", "2", "2", "2"]
+}];
